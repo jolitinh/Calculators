194b4b84a38118ff4f810161c7b447298cf6785e
Hassan, Jorge <jorge.hassan@mheducation.com>
2016-05-25 11:27:39 -0400

    adding static web server

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var connect = require('connect');
+var serve = require('serve-static');
 
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
@@ -9,6 +11,14 @@ gulp.task('styles', function() {
 
 gulp.task('watch',function() {
     gulp.watch('sass/**/*.scss',['styles']);
+});
+
+gulp.task('server', function() {
+  return connect().use(serve(__dirname))
+    .listen(8080)
+    .on('listening', function() {
+      console.log('Server Running: View at http://localhost:8080');
+      });
 });
 
 gulp.task('default', ['styles', 'watch']);
