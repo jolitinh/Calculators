606499616fefcbdb78cd35c70b32097f0b13286c
Hassan, Jorge <jorge.hassan@mheducation.com>
2016-06-09 16:02:57 -0400

    removed es6 syntax from spec

--- app/modules/calculator/test/spec/basic-calculator.spec.js
+++ app/modules/calculator/test/spec/basic-calculator.spec.js
@@ -1,12 +1,16 @@
-describe('A basic calculator', () => {
+describe('A basic calculator', function() {
   var calculatorElement, controller;
 
-  var createMockEvent = (value) => ({
-    currentTarget: {value}
-  });
+  var createMockEvent = function(value) {
+    return {
+      currentTarget: {
+        value: value
+      }
+    }
+  };
 
   beforeEach(module('dle.calculator'));
-  beforeEach(inject(($rootScope, $compile) => {
+  beforeEach(inject(function($rootScope, $compile) {
     var scope = $rootScope.$new();
     var calculatorElement = angular.element('<calculator></calculator>');
 
@@ -20,8 +24,8 @@ describe('A basic calculator', () => {
     expect(controller.calculatorOutput).toBe('0');
   });
 
-  describe('$scope.appendNumber appends to the output (display) string', () => {
-    it('dipslays the entered value', () => {
+  describe('$scope.appendNumber appends to the output (display) string', function() {
+    it('dipslays the entered value', function() {
       controller.appendNumber(createMockEvent('5'));
       expect(controller.calculatorOutput).toBe('5');
 
@@ -38,7 +42,7 @@ describe('A basic calculator', () => {
       expect(controller.calculatorOutput).toBe('568.4');
     });
 
-    it('does nothing to the output if a second decimal is entered', () => {
+    it('does nothing to the output if a second decimal is entered', function() {
       controller.appendNumber(createMockEvent('4'));
       expect(controller.calculatorOutput).toBe('4');
 
@@ -53,15 +57,15 @@ describe('A basic calculator', () => {
     })
   });
 
-  describe('$scope.performCalculation', () => {
-    it('does nothing when an operator hasn\'t been specified', () => {
+  describe('$scope.performCalculation', function() {
+    it('does nothing when an operator hasn\'t been specified', function() {
       controller.calculatorOutput = '4';
 
       controller.performCalculation();
       expect(controller.calculatorOutput).toBe('4');
     });
 
-    it('sets output to "Infinity" when dividing by 0', () => {
+    it('sets output to "Infinity" when dividing by 0', function() {
       controller.calculatorOutput = '5';
       controller.appendOperation(createMockEvent('/'));
       controller.appendNumber(createMockEvent('0'));
@@ -70,7 +74,7 @@ describe('A basic calculator', () => {
       expect(controller.calculatorOutput).toBe(Infinity);
     });
 
-    it('sets output to "Error" when dividing 0 by 0', () => {
+    it('sets output to "Error" when dividing 0 by 0', function() {
       controller.calculatorOutput = '0';
       controller.appendOperation(createMockEvent('/'));
       controller.appendNumber(createMockEvent('0'));
@@ -79,7 +83,7 @@ describe('A basic calculator', () => {
       expect(controller.calculatorOutput).toBe('Error');
     });
 
-    it('uses correct precision when adding decimals', () => {
+    it('uses correct precision when adding decimals', function() {
       controller.calculatorOutput = '.1';
       controller.appendOperation(createMockEvent('+'));
       controller.appendNumber(createMockEvent('.2'));
@@ -89,8 +93,8 @@ describe('A basic calculator', () => {
     });
   });
 
-  describe('$scope.appendOperation keeps track of an entered operator', () => {
-    it('if a new operator is entered when there are already two operands, it should go ahead and perform the operation', () => {
+  describe('$scope.appendOperation keeps track of an entered operator', function() {
+    it('if a new operator is entered when there are already two operands, it should go ahead and perform the operation', function() {
       controller.calculatorOutput = '1';
       controller.appendOperation(createMockEvent('+'));
       controller.appendNumber(createMockEvent('3'));
@@ -99,14 +103,14 @@ describe('A basic calculator', () => {
       expect(controller.calculatorOutput).toBe(4);
     });
 
-    it('should still display the original value if no operator has been previously specified', () => {
+    it('should still display the original value if no operator has been previously specified', function() {
       controller.calculatorOutput = '1';
       controller.appendOperation(createMockEvent('+'));
       expect(controller.calculatorOutput).toBe('1');
 
     });
 
-    it('if a new operator is entered when there is only one operand, replace the existing one', () => {
+    it('if a new operator is entered when there is only one operand, replace the existing one', function() {
       controller.appendNumber(createMockEvent('8'));
       controller.appendOperation(createMockEvent('+'));
       controller.appendOperation(createMockEvent('-'));
@@ -116,7 +120,7 @@ describe('A basic calculator', () => {
       expect(controller.calculatorOutput).toBe(3);
     });
 
-    it('if a new operator is entered when there is already one, the calculation should be done automatically', () => {
+    it('if a new operator is entered when there is already one, the calculation should be done automatically', function() {
       controller.appendNumber(createMockEvent('8'));
       controller.appendOperation(createMockEvent('+'));
       controller.appendNumber(createMockEvent('5'));
@@ -127,15 +131,15 @@ describe('A basic calculator', () => {
 
   });
 
-  describe('$scope.invertValue inverts the current value', () => {
-      it('positive values become negative', () => {
+  describe('$scope.invertValue inverts the current value', function() {
+      it('positive values become negative', function() {
         controller.calculatorOutput = '5';
 
         controller.invertValue();
         expect(controller.calculatorOutput).toBe(-5);
       });
 
-      it('negative values become positive', () => {
+      it('negative values become positive', function() {
         controller.calculatorOutput = '-5';
 
         controller.invertValue();
@@ -144,8 +148,8 @@ describe('A basic calculator', () => {
       });
   })
 
-  describe('$scope.squareRoot', () => {
-    it('changes current value to its square root', () => {
+  describe('$scope.squareRoot', function() {
+    it('changes current value to its square root', function() {
       controller.calculatorOutput = '16';
 
       controller.squareRoot();
@@ -153,8 +157,8 @@ describe('A basic calculator', () => {
     });
   });
 
-  describe('$scope.divideByHundred', () => {
-    it('display current value as a percentage', () => {
+  describe('$scope.divideByHundred', function() {
+    it('display current value as a percentage', function() {
       controller.calculatorOutput = '1605';
 
       controller.divideByHundred();
@@ -162,8 +166,8 @@ describe('A basic calculator', () => {
     });
   });
 
-  describe('$scope.clearEntries', () => {
-    it('clears operator, operands, and output', () => {
+  describe('$scope.clearEntries', function() {
+    it('clears operator, operands, and output', function() {
       controller.calculatorOutput = '1';
 
       controller.clearEntries();
