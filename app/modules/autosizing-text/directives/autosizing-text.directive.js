1185d1fa502c5023813853144ed9e9eeef8b8937
Hassan, Jorge <jorge.hassan@mheducation.com>
2016-07-07 17:41:07 -0400

    cleaning lint issues

--- app/modules/autosizing-text/directives/autosizing-text.directive.js
+++ app/modules/autosizing-text/directives/autosizing-text.directive.js
@@ -3,22 +3,22 @@ module.exports = function() {
 
   return {
     restrict: 'A',
-    link: function(scope, element, attrs) {
+    link: function(scope, element) {
       element.css('font-size', '36px');
 
       element.bind('DOMSubtreeModified',function() {
         console.log('offsetWidth:', this.offsetWidth);
         console.log('scrollWidth', this.scrollWidth);
         var outputField = angular.element(this);
-        var originalWidth = this.offsetWidth;
 
-        if(this.scrollWidth > this.offsetWidth){
-          while(this.scrollWidth > this.offsetWidth){
+        if (this.scrollWidth > this.offsetWidth) {
+          while (this.scrollWidth > this.offsetWidth) {
             var originalFontSize = parseInt(outputField.css('font-size'));
-            var smallerFont = outputField.css('font-size', originalFontSize - 1 + 'px');
+
+            outputField.css('font-size', originalFontSize - 1 + 'px');
           }
-        } else if((this.scrollWidth == this.offsetWidth) ) {
-            outputField.css('font-size', '36px');
+        } else if (this.scrollWidth === this.offsetWidth) {
+          outputField.css('font-size', '36px');
         }
       });
     }
--- dist/dle.utilities.js
+++ dist/dle.utilities.js
@@ -590,21 +590,21 @@
 
 	  return {
 	    restrict: 'A',
-	    link: function link(scope, element, attrs) {
+	    link: function link(scope, element) {
 	      element.css('font-size', '36px');
 
 	      element.bind('DOMSubtreeModified', function () {
 	        console.log('offsetWidth:', this.offsetWidth);
 	        console.log('scrollWidth', this.scrollWidth);
 	        var outputField = angular.element(this);
-	        var originalWidth = this.offsetWidth;
 
 	        if (this.scrollWidth > this.offsetWidth) {
 	          while (this.scrollWidth > this.offsetWidth) {
 	            var originalFontSize = parseInt(outputField.css('font-size'));
-	            var smallerFont = outputField.css('font-size', originalFontSize - 1 + 'px');
+
+	            outputField.css('font-size', originalFontSize - 1 + 'px');
 	          }
-	        } else if (this.scrollWidth == this.offsetWidth) {
+	        } else if (this.scrollWidth === this.offsetWidth) {
 	          outputField.css('font-size', '36px');
 	        }
 	      });
